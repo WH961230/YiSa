0e395091e18dd9cdc8072ca825a1d0bc03a3d2a9
Evoreek <Evoreek@outlook.com>
2024-02-20 11:22:47 +0800

    Initial commit

--- Assets/LazyPan/Scripts/GamePlay/Behaviour/Behaviour_RestoringBlood.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LazyPan {
-    public class Behaviour_RestoringBlood : Behaviour {
-        public Behaviour_RestoringBlood(Entity entity, string sign) : base(entity, sign) {
-        }
-    }
-}
--- Assets/LazyPan/Scripts/GamePlay/Door.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class Door : MonoBehaviour {
-    public static Door Instance;
-    public void Awake() {
-        Instance = this;
-    }
-}
